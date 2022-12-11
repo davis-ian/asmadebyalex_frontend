25fca865a6a44c9b360bdd7d346ae7edfc1b7bbf
ian <iandavis6481@gmail.com>
2022-12-11 11:09:09 -0600

    under mx update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
-      <TestComp />
+      <!-- <HelloWorld /> -->
+      <TestComp id="main" />
     </v-main>
   </v-app>
 </template>
@@ -11,3 +11,9 @@
 import HelloWorld from "@/components/HelloWorld.vue";
 import TestComp from "@/components/TestComponent.vue";
 </script>
+
+<style scoped>
+#main {
+  min-width: 100%;
+}
+</style>
