b2061004e8e3e7dca2cd0628fe73496bb8d14124
ian <iandavis6481@gmail.com>
2023-09-09 11:32:05 -0500

    featured max width

--- src/components/UI/FeaturedRecipes.vue
+++ src/components/UI/FeaturedRecipes.vue
@@ -8,23 +8,25 @@
       <!-- <div class="d-flex justify-center" style="gap: 40px">
         <div v-for="i in 3" style="width: 100%"> -->
 
-      <v-row>
-        <v-col v-for="i in 3" cols="12" md="4" sm="6">
-          <div class="pa-3">
-            <v-img
-              cover
-              aspect-ratio="1"
-              class="rounded-circle"
-              :src="placeholderImgSrc"
-              :lazy-src="placeholderImgSrc"
-            ></v-img>
-            <div class="text-center all-caps">
-              <h5 class="mb-0">Recipe</h5>
-              <p>$20</p>
+      <div class="d-flex justify-center">
+        <v-row style="max-width: 1200px">
+          <v-col v-for="i in 3" cols="12" md="4" sm="6">
+            <div class="pa-3">
+              <v-img
+                cover
+                aspect-ratio="1"
+                class="rounded-circle"
+                :src="placeholderImgSrc"
+                :lazy-src="placeholderImgSrc"
+              ></v-img>
+              <div class="text-center all-caps">
+                <h5 class="mb-0">Recipe</h5>
+                <p>$20</p>
+              </div>
             </div>
-          </div>
-        </v-col>
-      </v-row>
+          </v-col>
+        </v-row>
+      </div>
       <!-- </div>
       </div> -->
       <div class="text-center pa-5">
