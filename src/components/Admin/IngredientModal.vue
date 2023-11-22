12e34c151f26f709bd829f71122e08206f0aec43
ian <iandavis6481@gmail.com>
2023-11-22 07:16:30 -0600

    added delete ingredient

--- src/components/Admin/IngredientModal.vue
+++ src/components/Admin/IngredientModal.vue
@@ -2,7 +2,14 @@
   <div>
     <v-dialog v-model="display" max-width="650px">
       <v-card :loading="loading">
-        <v-card-title>Ingredients</v-card-title>
+        <v-card-title>
+          <div class="d-flex justify-space-between">
+            <span> Ingredients </span>
+            <v-btn variant="outlined" @click="newIngredientModal = true"
+              >New +</v-btn
+            >
+          </div>
+        </v-card-title>
         <v-card-text>
           <div class="text-center" v-if="ingredients.length == 0">
             <p v-if="loading">Loading results..</p>
@@ -14,8 +21,9 @@
               <template v-slot:append>
                 <div style="gap: 10px" class="d-flex">
                   <v-btn
+                    :disabled="loading"
                     @click="initEdit(item)"
-                    variant="tonal"
+                    variant="outlined"
                     class="xs-btn"
                     icon
                   >
@@ -23,7 +31,14 @@
                       icon="fa-solid fa-pencil"
                     ></font-awesome-icon>
                   </v-btn>
-                  <v-btn variant="tonal" color="error" class="xs-btn" icon>
+                  <v-btn
+                    :disabled="loading"
+                    @click="deleteIngredient1(item)"
+                    variant="outlined"
+                    color="error"
+                    class="xs-btn"
+                    icon
+                  >
                     <font-awesome-icon
                       icon="fa-solid fa-trash"
                     ></font-awesome-icon>
@@ -33,9 +48,6 @@
             </v-list-item>
           </v-list>
         </v-card-text>
-        <v-card-actions>
-          <v-btn @click="newIngredientModal = true">New +</v-btn>
-        </v-card-actions>
       </v-card>
     </v-dialog>
 
@@ -46,18 +58,8 @@
           <v-text-field label="name" v-model="newIngredientName"></v-text-field>
         </v-card-text>
         <v-card-actions>
-          <v-btn
-            :loading="loading"
-            :disabled="loading"
-            @click="cancelNewIngredient"
-            >Cancel</v-btn
-          >
-          <v-btn
-            :loading="loading"
-            :disabled="loading"
-            @click="submitNewIngredient"
-            >Submit</v-btn
-          >
+          <v-btn :disabled="loading" @click="cancelNewIngredient">Cancel</v-btn>
+          <v-btn :disabled="loading" @click="submitNewIngredient">Submit</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -73,16 +75,32 @@
           ></v-text-field>
         </v-card-text>
         <v-card-actions>
-          <v-btn
-            :loading="loading"
-            :disabled="loading"
-            @click="cancelEditIngredient"
+          <v-btn :disabled="loading" @click="cancelEditIngredient"
             >Cancel</v-btn
           >
-          <v-btn
-            :loading="loading"
-            :disabled="loading"
-            @click="submitEditIngredient"
+          <v-btn :disabled="loading" @click="submitEditIngredient"
+            >Submit</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="deleteModal" max-width="400px">
+      <v-card :loading="loading">
+        <v-card-title>Delete Ingredient?</v-card-title>
+        <v-card-text>
+          Are you sure you want to delete this ingredient? This cannot be
+          undone.
+          <v-divider></v-divider>
+          <div>
+            <strong>{{ tempIngredient.name }}</strong>
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn :disabled="loading" @click="cancelDeleteIngredient"
+            >Cancel</v-btn
+          >
+          <v-btn :disabled="loading" @click="submitDeleteIngredient"
             >Submit</v-btn
           >
         </v-card-actions>
@@ -103,6 +121,7 @@ export default {
       tempIngredient: null,
       editing: false,
       loading: true,
+      deleteModal: false,
     };
   },
   watch: {
@@ -116,6 +135,53 @@ export default {
     ...mapStores(useSnackbarStore),
   },
   methods: {
+    submitDeleteIngredient() {
+      if (!this.tempIngredient.id) {
+        this.snackbarStore.showSnackbar({
+          message: "Ingredient id cannot be empty",
+        });
+        return;
+      }
+
+      this.loading = true;
+
+      this.axiosInstance
+        .delete(`/ingredients/${this.tempIngredient.id}`)
+        .then((res) => {
+          console.log(res.data, "ingredient response");
+
+          let found = this.ingredients.find(
+            (x) => x.id == this.tempIngredient.id
+          );
+          if (found) {
+            let index = this.ingredients.indexOf(found);
+            this.ingredients.splice(index, 1);
+          }
+
+          this.snackbarStore.showSnackbar({ message: "Ingredient deleted" });
+        })
+        .catch((err) => {
+          console.log(err, "error");
+          this.snackbarStore.showSnackbar({
+            message: "Error deleting ingredient",
+            color: "error",
+          });
+        })
+        .finally(() => {
+          this.loading = false;
+          this.cancelDeleteIngredient();
+        });
+    },
+    cancelDeleteIngredient() {
+      this.deleteModal = false;
+      setTimeout(() => {
+        this.tempIngredient = null;
+      }, 500);
+    },
+    deleteIngredient1(item) {
+      this.tempIngredient = item;
+      this.deleteModal = true;
+    },
     submitEditIngredient() {
       console.log(this.tempIngredient, "submit edit");
 
